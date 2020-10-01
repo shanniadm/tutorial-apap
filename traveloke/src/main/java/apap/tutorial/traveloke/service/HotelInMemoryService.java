a643793219f7d0eb767ddee6c96f4525ca40523f
shanniadm <shannia.dwim@gmail.com>
2020-10-01 11:26:07 +0700

    feat/tutorial-2 add class HotelInMemoryService

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/service/HotelInMemoryService.java
@@ -0,0 +1,39 @@
+package apap.tutorial.traveloke.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import apap.tutorial.traveloke.model.HotelModel;
+
+@Service
+public class HotelInMemoryService implements HotelService{
+    
+    private List<HotelModel> listHotel;
+
+    //Constructor 
+    public HotelInMemoryService(){
+        listHotel = new ArrayList<>();
+    }
+
+    @Override
+    public void addHotel(HotelModel hotel){
+        listHotel.add(hotel);
+    }
+
+    @Override
+    public List<HotelModel> getHotelList(){
+        return listHotel;
+    }
+
+    @Override
+    public HotelModel getHotelByIdHotel(String idHotel){
+        for(HotelModel hotel : listHotel){
+            if(hotel.getIdHotel().equals(idHotel)){
+                return hotel;
+            }
+        }
+    }
+
+}
