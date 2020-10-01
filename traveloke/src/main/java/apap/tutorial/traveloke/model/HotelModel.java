ac2c0de3d65378fab34667505bb50fcb5ed69781
shanniadm <shannia.dwim@gmail.com>
2020-10-01 10:45:11 +0700

    feat:tutorial 2 - create model HotelModel

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/model/HotelModel.java
@@ -0,0 +1,50 @@
+package apap.tutorial.traveloke.model;
+
+public class HotelModel {
+    private String idHotel;
+    private String namaHotel;
+    private String alamat;
+    private String noTelepon; 
+
+    //Generate
+    public HotelModel(String idHotel, String namaHotel, String alamat, String noTelepon){
+        this.idHotel = idHotel;
+        this.namaHotel = namaHotel;
+        this.alamat = alamat;
+        this.noTelepon = noTelepon;
+    }
+
+    //Getter untuk idHotel, namaHotel, alamat, dan noTelepon.
+    public String getIdHotel(){
+        return idHotel;
+    }
+
+    public String getNamaHotel(){
+        return namaHotel;
+    }
+
+    public String getAlamat(){
+        return alamat;
+    }
+
+    public String getNoTelepon(){
+        return noTelepon;
+    }
+
+    //Setter untuk idHotel, namaHotel, alamat, dan noTelepon.
+    public void setIdHotel(String idHotel){
+        this.idHotel = idHotel;
+    }
+
+    public void setNamaHotel(String namaHotel){
+        this.namaHotel = namaHotel;
+    }
+
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+
+    public void setNoTelepon(String noTelepon){
+        this.noTelepon = noTelepon;
+    }
+}
