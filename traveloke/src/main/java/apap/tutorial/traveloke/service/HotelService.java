18da06e46673dd3f44cb9ee23452585730bf7907
shanniadm <shannia.dwim@gmail.com>
2020-10-01 10:58:56 +0700

    feat:tutorial 2 - make interface HotelService

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/service/HotelService.java
@@ -0,0 +1,14 @@
+package apap.tutorial.traveloke.service;
+import apap.tutorial.traveloke.model.HotelModel;
+import java.util.List;
+
+public interface HotelService {
+    //Method untuk menambah Hotel
+    void addHotel(HotelModel hotel);
+
+    //Method untuk mendapatkan daftar Hotel yang telah tersimpan
+    List<HotelModel> getHotelList();
+
+    //Method untuk mendapatkan data sebuah hotel berdasarkan idHotel 
+    HotelModel getHotelByIdHotel(String idHotel);
+}
