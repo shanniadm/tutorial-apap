07d3cdbd29564dfb21c9798b7bd5f37f262fe753
shanniadm <shannia.dwim@gmail.com>
2020-11-19 10:30:12 +0700

    fixed

--- traveloke/src/main/java/apap/tutorial/traveloke/controller/KamarController.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/KamarController.java
@@ -8,20 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class KamarController {
-    HotelTemp hotelTemp = new HotelTemp();
-
     @Qualifier("hotelServiceImpl")
     @Autowired
     private HotelService hotelService;
@@ -29,32 +31,57 @@ public class KamarController {
     @Autowired
     private KamarService kamarService;
 
-    @GetMapping("/kamar/add/{idHotel}")
-    private String addKamarFormPage(
-        @PathVariable Long idHotel,
-        Model model
-    ){
-        try{
-            if(hotelTemp.getKamar().size()==1){
-                hotelTemp.getKamar().get(0).setHotel(hotelService.getHotelByIdHotel(idHotel));
-            };
-            model.addAttribute("hotel", hotelTemp);
-            model.addAttribute("halaman", "Add Kamar");
-            return "form-add-kamar";
-        } catch (Exception e){
-            model.addAttribute("idProcess", idHotel);
-            return "cant-process";
+    @RequestMapping(value = "/kamar/add/{idHotel}")
+    private String add(@PathVariable(value = "idHotel") Long idHotel, Model model) {
+        model.addAttribute("titleTab", "Add Kamar");
+        HotelModel hotel = new HotelModel();
+        hotel.setListKamar(new ArrayList<KamarModel>());
+        hotel.getListKamar().add(new KamarModel());
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("idHotel", idHotel);
+        return "form-add-kamar";
+    }
+
+    @RequestMapping(value = "/kamar/add/{idHotel}", method = RequestMethod.POST, params= {"save"})
+    private String addMenuSubmit(@PathVariable(value = "idHotel") Long idHotel, @ModelAttribute HotelModel hotel, ModelMap model) {
+        model.addAttribute("titleTab", "Add Menu");
+        HotelModel oldHotel = hotelService.getHotelByIdHotel(idHotel);
+        model.addAttribute("kamarCount", hotel.getListKamar().size());
+
+        model.addAttribute("idHotel", idHotel);
+        for (KamarModel kamar : hotel.getListKamar()) {
+            kamar.setHotel(oldHotel);
+            kamarService.addKamar(kamar);
         }
+        return "add-kamar";
+    }
+
+    @RequestMapping(value = "/kamar/add/{idHotel}", method = RequestMethod.POST, params={"addRow"})
+    private String addRow(@PathVariable(value = "idHotel") Long idHotel, @ModelAttribute HotelModel hotel, Model model) {
+        model.addAttribute("titleTab", "Add Menu");
+        KamarModel kamar = new KamarModel();
+        hotel.getListKamar().add(kamar);
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("idHotel", idHotel);
+        return "form-add-kamar";
+    }
+
+    @RequestMapping(value = "/kamar/add/{idHotel}", method = RequestMethod.POST, params={"removeRow"})
+    private String removeRow(@PathVariable(value = "idHotel") Long idHotel,@ModelAttribute HotelModel hotel, Model model, HttpServletRequest req) {
+        Integer rowId =  Integer.valueOf(req.getParameter("removeRow"));
+        hotel.getListKamar().remove(rowId.intValue());
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("idHotel", idHotel);
+        return "form-add-kamar";
     }
 
     @PostMapping("/kamar/add")
     private String addKamarSubmit(
-        @ModelAttribute HotelTemp hotel, 
-        Model model
+            @ModelAttribute KamarModel kamar,
+            Model model
     ){
-        for(KamarModel km : hotel.getKamar()){
-            kamarService.addKamar(km);
-        }
+        kamarService.addKamar(kamar);
+        model.addAttribute("kamar", kamar);
         return "add-kamar";
     }
 
@@ -100,42 +127,7 @@ public class KamarController {
         return "delete-kamar";
     }
 
-    // @GetMapping("/kamar/delete/{noKamar}")
-    // public String deleteHotel(
-    //     @PathVariable Long noKamar, 
-    //     Model model
-    // ){
-    //     try{
-    //         Long idHotel = kamarService.deleteKamar(noKamar);
-    //         model.addAttribute("noKamar", noKamar);
-    //         model.addAttribute("idHotel", idHotel);
-    //         return "delete-kamar";
-    //     } catch (Exception e){
-    //         model.addAttribute("idProcess", noKamar);
-    //         return "cant-process";
-    //     }
-        
-    // }
 }
-
-class HotelTemp{
-    KamarModel km = new KamarModel();
-    private List<KamarModel> kamar = Arrays.asList(km);
-
-    public void addList(KamarModel kamar){
-        this.kamar.add(kamar);
-    }
-
-    public List<KamarModel> getKamar(){
-        return this.kamar;
-    }
-
-    public void setList(List<KamarModel> kamar){
-        this.kamar = kamar;
-    }
-
-}
-
 
 
 
